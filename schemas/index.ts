04d6423af06e140be8fc1aea0e5bc8da844228ff
suyogmungale <suyogmungale5@gmail.com>
2023-05-23 11:02:18 +0530

    initial

---
+++ schemas/index.ts
@@ -0,0 +1,11 @@
+import testimonialSchema from './testimonials'
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skill from './skills'
+import work from './work'
+import workexperience from './workexperience'
+
+
+export const schemaTypes = [testimonialSchema, about, brands, contact, experiences, skill, work, workexperience]
---
+++ schemas/testimonials.ts
@@ -0,0 +1,33 @@
+const testimonialSchema = {
+    name: "testinomials",
+    title: "Testimonials",
+    type: "document",
+    fields: [
+      {
+        name: "name",
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: "company",
+        title: 'Company',
+        type: 'string',
+      },
+      {
+        name: "imageurl",
+        title: 'ImgURL',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: "feedback",
+        title: 'Feedback',
+        type: 'string',
+      },
+    ],
+  };
+  
+  export default testimonialSchema;
+  
